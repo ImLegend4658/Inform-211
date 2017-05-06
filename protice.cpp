0da993baa2dc393599f2aeeab733b555eac45baf
MrAziz2009 <Cool_zizo2009@hotmail.com>
2017-05-06 02:21:02 -0400

    Update protice.cpp

--- protice.cpp
+++ protice.cpp
@@ -54,3 +54,78 @@ int main(){
 	system("pause"); 
 
 }
+
+
+====================================================================================================
+	=======================================================================================================
+	========================================================================================
+	
+	with functions <><><>
+	
+	#include <iostream>
+using namespace std; 
+/*
+protice with functions codes of fractions 
+#2
+*/
+int SomeCulclate(double &demon,  double &numon, float result);
+void addTwoNum(double &demon, double &numon);
+int main(){
+
+	double demon=0;
+	double numon=0;
+	float result=0;
+
+	addTwoNum(demon, numon);
+	SomeCulclate(demon, numon, result);
+
+	system("pause"); 
+
+}
+
+void addTwoNum(double &demon, double &numon){
+
+	cout << "please enter the Demonater number" << endl; 
+	cin >> demon;
+
+	cout << "please enter the numonator number" << endl; 
+	cin >> numon;
+}
+
+int SomeCulclate(double &demon, double &numon, float result){
+
+	int choicNum;
+	cout << "welcome to our program fruction :)" << endl
+		<< "please choice the number what you need" << endl
+		<< " Addition, press number <1>" << endl
+		<< "Subtruction, press number <2>" << endl
+		<< "Mulity, press number <3>" << endl
+		<< "divition, press number <4>" << endl;
+	cin >> choicNum;
+
+	switch (choicNum)
+	{
+	case 1:
+		result = (demon / numon) + (numon / demon);
+
+		cout << result << " Fraction on addtion is: " << endl;
+		break;
+	case 2:
+		result = (demon / numon) - (numon / demon);
+		cout << result << " Fraction on substract is: " << endl;
+		break;
+	case 3:
+		result = (demon / numon) * (numon / demon);
+		cout << result << " Fraction on multi is: " << endl;
+		break;
+	case 4:
+		result = (demon / numon) / (numon / demon);
+		cout << result << " Fraction on multi is: " << endl;
+		break;
+	default:
+		cout << "error" << endl;
+		exit(1);
+	}
+	return result;
+
+}
