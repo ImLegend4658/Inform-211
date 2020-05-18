cb6c7c2646b7de6cb6f018dc59729f6acf8e615f
ImLegend4658 <MrAziz2005@gmail.com>
2020-05-19 00:08:05 +0300

    Update write.cpp

--- write.cpp
+++ write.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include<filesystem>
 using namespace std; 
 
 int main (){
-
+bool exis = true; 
 ifstream input;
 char num;
-
-input.open("README.txt");
-
+string filenames;
+cout<<"Enter filename to read it: "<<endl; 
+cin>>filenames;
+input.open(filenames+".txt");
+if(input.fail()){
+    cout<<"doesnt exsit"<<endl;
+    exit(0);
+}
 while (! input.eof()){
 
 input.get(num);
 
 cout<<num; 
-
 
 }
 
